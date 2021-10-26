1d59915766802e120628f078c13e1fc28852a710
Ajay Singh <daltavida@gmail.com>
2021-10-26 13:50:00 +1030

    Link leaflet library for geolocation

--- script.js
+++ script.js
@@ -14,7 +14,18 @@ const inputElevation = document.querySelector(".form__input--elevation");
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
-    console.log(latitude, longitude);
+
+    var map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([51.5, -0.09])
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
   },
   function () {
     alert("Error: Could not get your location");
