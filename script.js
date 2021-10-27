59aab222ffe92f8a0ed205a2e429c53c28fa7727
Ajay Singh <daltavida@gmail.com>
2021-10-27 14:51:00 +1030

    Show a map and marker at user's current loaction

--- script.js
+++ script.js
@@ -14,15 +14,16 @@ const inputElevation = document.querySelector(".form__input--elevation");
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
 
-    var map = L.map("map").setView([51.505, -0.09], 13);
+    const map = L.map("map").setView(coords, 15);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker([51.5, -0.09])
+    L.marker(coords)
       .addTo(map)
       .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
       .openPopup();
