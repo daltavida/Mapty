5c5962bcb2ffa2817012707696cfb006d00f4f76
Ajay Singh <daltavida@gmail.com>
2021-10-27 14:51:00 +1030

    Add a marker when user clicks on the map

--- script.js
+++ script.js
@@ -23,10 +23,10 @@ navigator.geolocation.getCurrentPosition(
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-      .openPopup();
+    map.on("click", function (mapEvent) {
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng]).addTo(map).bindPopup("Workout").openPopup();
+    });
   },
   function () {
     alert("Error: Could not get your location");
