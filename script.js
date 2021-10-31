72246a4ee1f13b04c209e3a1ca17ea535d223270
Ajay Singh <daltavida@gmail.com>
2021-10-31 18:55:00 +1030

    Finish refactoring

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ class App {
 
   constructor() {
     this._getPosition();
-
     form.addEventListener("submit", this._newWorkout.bind(this));
+    inputType.addEventListener("change", this._toggleElevationField());
   }
 
   _getPosition() {
@@ -41,16 +41,19 @@ class App {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
-    this.#map.on("click", function (mapE) {
-      this.#mapEvent = mapE;
-      form.classList.remove("hidden");
-      inputDistance.focus();
-    });
+    this.#map.on("click", this._showForm.bind(this));
   }
 
-  _showForm() {}
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
 
-  _toggleElevationField() {}
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
 
   _newWorkout(e) {
     e.preventDefault();
@@ -79,8 +82,3 @@ class App {
 }
 
 const app = new App();
-
-inputType.addEventListener("change", function () {
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
