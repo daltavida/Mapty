42e1a2b556f2ad0fc3014c5783b571e07695d696
Ajay Singh <daltavida@gmail.com>
2021-11-07 12:01:00 +1030

    Show workout description on marker

--- script.js
+++ script.js
@@ -186,7 +186,9 @@ class App {
           className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent(workout.distance)
+      .setPopupContent(
+        `${workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"} ${workout.description}`
+      )
       .openPopup();
   }
 
